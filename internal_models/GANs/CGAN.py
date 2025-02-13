2bf183531dae140feecc9404505a8a2b7a914297
Nick Askari <nicka@ntnu.no>
2025-02-13 18:12:08 +0100

    gans:are:hard

--- internal_models/GANs/CGAN.py
+++ internal_models/GANs/CGAN.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import numpy as np
 import math
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
@@ -14,7 +18,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-os.makedirs("images", exist_ok=True)
+# THE C IS MISSING GAN - THE GAN IS FUCKED
+
+os.makedirs("generated_returns", exist_ok=True)
+# THIS IS CURRENTLY NOT A CGAN, BUT A NORMAL GAN
+
+# LOADING DATA SET FROM RETURNS_DF
+returns_df = pd.read_csv('../../data/final_daily_returns_asset_classes.csv', index_col=0, parse_dates=True)
+
+# ROLLING RETURNS
+window_size = 252  
+
+rolling_returns = []
+for i in range(len(returns_df) - window_size):
+    window = returns_df.iloc[i:i + window_size].values.flatten()  # Flatten to create a 1D vector
+    rolling_returns.append(window)
+
+rolling_returns = np.array(rolling_returns)
+
+
+
+# DEFINING PARAMTERS
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
@@ -24,14 +48,14 @@ parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first 
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--n_assets", type=int, default=7, help="number of assets in the portfolio")
+parser.add_argument("--window_size", type=int, default=252, help="size of the rolling window in days (1 year)")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between sampling generated return sequences")
 opt = parser.parse_args()
 print(opt)
 
-img_shape = (opt.channels, opt.img_size, opt.img_size)
+# Each sample is a 1-year return sequence for 7 assets
+input_shape = (opt.window_size * opt.n_assets,)  # 252 * 7 = 1764 features
 
 cuda = True if torch.cuda.is_available() else False
 
@@ -39,9 +63,7 @@ cuda = True if torch.cuda.is_available() else False
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-
-        self.label_emb = nn.Embedding(opt.n_classes, opt.n_classes)
-
+        
         def block(in_feat, out_feat, normalize=True):
             layers = [nn.Linear(in_feat, out_feat)]
             if normalize:
@@ -49,46 +71,42 @@ class Generator(nn.Module):
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
 
+        # Consists of 4 layers
         self.model = nn.Sequential(
-            *block(opt.latent_dim + opt.n_classes, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
+            *block(opt.latent_dim, 512, normalize=False),
             *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
+            *block(1024, 2048),
+            nn.Linear(2048, int(np.prod(input_shape))),
         )
 
-    def forward(self, noise, labels):
-        # Concatenate label embedding and image to produce input
-        gen_input = torch.cat((self.label_emb(labels), noise), -1)
-        img = self.model(gen_input)
-        img = img.view(img.size(0), *img_shape)
-        return img
+
+    # Propagates noise input through the network
+    def forward(self, noise):
+        returns = self.model(noise)
+        returns = returns.view(returns.size(0), *input_shape)
+        return returns
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.label_embedding = nn.Embedding(opt.n_classes, opt.n_classes)
-
+        # This has three layers
         self.model = nn.Sequential(
-            nn.Linear(opt.n_classes + int(np.prod(img_shape)), 512),
+            nn.Linear(int(np.prod(input_shape)), 1024),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 512),
-            nn.Dropout(0.4),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
             nn.Linear(512, 1),
+            nn.Sigmoid(),  # Outputs a probability (real/fake)
         )
 
-    def forward(self, img, labels):
-        # Concatenate label embedding and image to produce input
-        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
-        validity = self.model(d_in)
+    def forward(self, returns):
+        validity = self.model(returns.view(returns.size(0), -1))
         return validity
+
 
 
 # Loss functions
@@ -98,25 +116,21 @@ adversarial_loss = torch.nn.MSELoss()
 generator = Generator()
 discriminator = Discriminator()
 
+# For Vizualization
+print(generator)
+print(discriminator)
+
 if cuda:
     generator.cuda()
     discriminator.cuda()
     adversarial_loss.cuda()
 
-# Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
+
+from torch.utils.data import TensorDataset, DataLoader
+
+rolling_returns_tensor = torch.tensor(rolling_returns, dtype=torch.float32)
+dataloader = DataLoader(TensorDataset(rolling_returns_tensor), batch_size=opt.batch_size, shuffle=True)
+
 
 # Optimizers
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
@@ -126,49 +140,35 @@ FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
-def sample_image(n_row, batches_done):
-    """Saves a grid of generated digits ranging from 0 to n_classes"""
-    # Sample noise
-    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
-    # Get labels ranging from 0 to n_classes for n rows
-    labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(LongTensor(labels))
-    gen_imgs = generator(z, labels)
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
-
-
 # ----------
 #  Training
 # ----------
 
 for epoch in range(opt.n_epochs):
-    for i, (imgs, labels) in enumerate(dataloader):
+    for i, (real_returns,) in enumerate(dataloader):
 
-        batch_size = imgs.shape[0]
+        batch_size = real_returns.size(0)
 
-        # Adversarial ground truths
-        valid = Variable(FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
-        fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
+        # Ground truths
+        valid = torch.ones((batch_size, 1), requires_grad=False).to(real_returns.device)
+        fake = torch.zeros((batch_size, 1), requires_grad=False).to(real_returns.device)
 
         # Configure input
-        real_imgs = Variable(imgs.type(FloatTensor))
-        labels = Variable(labels.type(LongTensor))
+        real_returns = real_returns.to(real_returns.device)
 
         # -----------------
         #  Train Generator
         # -----------------
-
         optimizer_G.zero_grad()
 
-        # Sample noise and labels as generator input
-        z = Variable(FloatTensor(np.random.normal(0, 1, (batch_size, opt.latent_dim))))
-        gen_labels = Variable(LongTensor(np.random.randint(0, opt.n_classes, batch_size)))
+        # Sample noise as generator input
+        z = torch.randn(batch_size, opt.latent_dim).to(real_returns.device)
 
-        # Generate a batch of images
-        gen_imgs = generator(z, gen_labels)
+        # Generate a batch of synthetic returns
+        gen_returns = generator(z)
 
         # Loss measures generator's ability to fool the discriminator
-        validity = discriminator(gen_imgs, gen_labels)
+        validity = discriminator(gen_returns)
         g_loss = adversarial_loss(validity, valid)
 
         g_loss.backward()
@@ -177,15 +177,14 @@ for epoch in range(opt.n_epochs):
         # ---------------------
         #  Train Discriminator
         # ---------------------
-
         optimizer_D.zero_grad()
 
-        # Loss for real images
-        validity_real = discriminator(real_imgs, labels)
+        # Real loss
+        validity_real = discriminator(real_returns)
         d_real_loss = adversarial_loss(validity_real, valid)
 
-        # Loss for fake images
-        validity_fake = discriminator(gen_imgs.detach(), gen_labels)
+        # Fake loss
+        validity_fake = discriminator(gen_returns.detach())
         d_fake_loss = adversarial_loss(validity_fake, fake)
 
         # Total discriminator loss
@@ -194,11 +193,25 @@ for epoch in range(opt.n_epochs):
         d_loss.backward()
         optimizer_D.step()
 
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
+        print(f"[Epoch {epoch}/{opt.n_epochs}] [Batch {i}/{len(dataloader)}] [D loss: {d_loss.item()}] [G loss: {g_loss.item()}]")
 
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            sample_image(n_row=10, batches_done=batches_done)
+        # Save generated returns periodically
+        if (epoch * len(dataloader) + i) % opt.sample_interval == 0:
+            torch.save(gen_returns, f"generated_returns/returns_{epoch}_{i}.pt")
+
+
+# Load generated returns
+gen_returns = torch.load('generated_returns/returns_0_0.pt')  # Example file
+# Reshape gen_returns to (batch_size, 252 days, 7 assets)
+gen_returns = gen_returns.view(gen_returns.size(0), 252, 7)
+gen_returns = gen_returns.cpu().detach().numpy()
+print(gen_returns.shape)
+
+# Plot distribution for each asset
+for asset in range(8):
+    plt.figure(figsize=(8, 4))
+    sns.histplot(gen_returns[:, :, asset].flatten(), bins=50, kde=True)
+    plt.title(f'Distribution of Returns for Asset {asset+1}')
+    plt.xlabel('Return')
+    plt.ylabel('Frequency')
+    plt.show()
---
+++ internal_models/GANs/WGAN.py
@@ -0,0 +1,188 @@
+import argparse
+import os
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
+from torch.autograd import Variable
+from sklearn.preprocessing import StandardScaler
+
+class WGAN_GP:
+
+    def __init__(self, returns_df, asset_name):
+        self.returns_df = returns_df
+        self.asset_name = asset_name
+        os.makedirs(f"generated_returns_{self.asset_name}", exist_ok=True)
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
+        parser.add_argument("--batch_size", type=int, default=200, help="size of the batches")
+        parser.add_argument("--lr", type=float, default=0.00002, help="learning rate for RMSprop")
+        parser.add_argument("--latent_dim", type=int, default=200, help="dimensionality of the latent space")
+        parser.add_argument("--window_size", type=int, default=252, help="size of the rolling window in days (1 year)")
+        parser.add_argument("--sample_interval", type=int, default=400, help="interval between sampling generated return sequences")
+        parser.add_argument("--lambda_gp", type=float, default=20, help="gradient penalty coefficient")
+        
+        opt, _ = parser.parse_known_args()
+        self.opt = opt
+
+        if isinstance(returns_df, pd.Series):
+            returns_df = returns_df.to_frame()
+        self.rolling_returns, self.scaler = self.create_rolling_returns(returns_df)
+        self.input_shape = (opt.window_size,)
+        self.cuda = torch.cuda.is_available()
+
+        self.generator = Generator(opt, self.input_shape)
+        self.critic = Critic(self.input_shape)
+
+        self.dataloader = None
+        self.optimizer_G = None
+        self.optimizer_C = None
+
+    def create_rolling_returns(self, returns_df):
+        window_size = self.opt.window_size
+        scaler = StandardScaler()
+        scaled_returns = scaler.fit_transform(returns_df.values.reshape(-1, 1))
+
+        rolling_returns = []
+        for i in range(len(scaled_returns) - window_size):
+            window = scaled_returns[i:i + window_size]
+            rolling_returns.append(window)
+        return np.array(rolling_returns), scaler
+
+    def compute_gradient_penalty(self, real_samples, fake_samples):
+        alpha = torch.rand(real_samples.size(0), 1, 1).to(real_samples.device)
+        
+        # Reshape fake_samples to match real_samples
+        fake_samples = fake_samples.view(fake_samples.size(0), real_samples.size(1), -1)
+
+        interpolates = (alpha * real_samples + (1 - alpha) * fake_samples).requires_grad_(True)
+        d_interpolates = self.critic(interpolates)
+        
+        gradients = torch.autograd.grad(outputs=d_interpolates, inputs=interpolates,
+                                        grad_outputs=torch.ones_like(d_interpolates),
+                                        create_graph=True, retain_graph=True, only_inputs=True)[0]
+        gradients = gradients.view(gradients.size(0), -1)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+        return gradient_penalty
+
+
+    def generate_scenarios(self, num_scenarios=1000):
+        self.generator.eval()  # Set generator to evaluation mode
+
+        all_generated_returns = []
+        batch_size = 1000  # Generate in batches to avoid memory issues
+
+        with torch.no_grad():
+            for _ in range(num_scenarios // batch_size):
+                z = torch.normal(0, 0.02, size=(batch_size, self.opt.latent_dim)).to('cuda' if self.cuda else 'cpu')
+                gen_returns = self.generator(z).cpu().numpy()
+
+                # Reshape for inverse transform
+                gen_returns = self.scaler.inverse_transform(gen_returns.reshape(gen_returns.shape[0], -1))  
+                gen_returns = gen_returns.reshape(-1, self.opt.window_size, 1)  # Reshape back to original format
+
+                all_generated_returns.append(gen_returns)
+
+        # Concatenate all batches into a single array
+        all_generated_returns = np.vstack(all_generated_returns)
+        np.save(f'generated_returns_{self.asset_name}/final_50000_scenarios.npy', all_generated_returns)
+
+
+    def setup(self):
+        if self.cuda:
+            self.generator.cuda()
+            self.critic.cuda()
+
+        rolling_returns_tensor = torch.tensor(self.rolling_returns, dtype=torch.float32)
+        self.dataloader = DataLoader(TensorDataset(rolling_returns_tensor), batch_size=self.opt.batch_size, shuffle=True)
+
+        self.optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=self.opt.lr, betas=(0.5, 0.9))
+        self.optimizer_C = torch.optim.Adam(self.critic.parameters(), lr=self.opt.lr, betas=(0.5, 0.9))
+
+    def train(self):
+        self.setup()
+
+        n_critic = 5  # Reduce to balance generator updates
+        for epoch in range(self.opt.n_epochs):
+            for i, (real_returns,) in enumerate(self.dataloader):
+                batch_size = real_returns.size(0)
+
+                # Train Critic
+                self.optimizer_C.zero_grad()
+                z = torch.normal(0, 0.2, size=(batch_size, self.opt.latent_dim)).to(real_returns.device)
+                gen_returns = self.generator(z)
+                real_validity = self.critic(real_returns)
+                fake_validity = self.critic(gen_returns.detach())
+                gradient_penalty = self.compute_gradient_penalty(real_returns, gen_returns)
+                c_loss = -torch.mean(real_validity) + torch.mean(fake_validity) + self.opt.lambda_gp * gradient_penalty
+
+                c_loss.backward()
+                self.optimizer_C.step()
+
+                # Train Generator every n_critic steps
+                if i % n_critic == 0:
+                    self.optimizer_G.zero_grad()
+                    gen_returns = self.generator(z)
+                    fake_validity = self.critic(gen_returns)
+                    g_loss = -torch.mean(fake_validity)
+
+                    g_loss.backward()
+                    self.optimizer_G.step()
+
+                if i % 10 == 0:
+                    print(f"[Epoch {epoch}/{self.opt.n_epochs}] [Batch {i}/{len(self.dataloader)}] [C loss: {c_loss.item()}] [G loss: {g_loss.item()}]")
+
+                if (epoch * len(self.dataloader) + i) % self.opt.sample_interval == 0:
+                    torch.save(gen_returns, f"generated_returns_{self.asset_name}/returns_{epoch}_{i}.pt")
+
+class Generator(nn.Module):
+    def __init__(self, opt, input_shape):
+        super(Generator, self).__init__()
+        self.opt = opt
+        self.input_shape = input_shape
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))  # Normalize for stability
+            layers.append(nn.LeakyReLU(0.2, inplace=True))  # Changed from ReLU to LeakyReLU
+            return layers
+
+        self.model = nn.Sequential(
+            *block(self.opt.latent_dim, 512),
+            *block(512, 512),
+            *block(512, 512),
+            *block(512, 512),
+            nn.Linear(512, int(np.prod(self.input_shape))),
+            nn.Tanh()  # Helps with stable value scaling
+        )
+
+    def forward(self, noise):
+        returns = self.model(noise)
+        return returns.view(returns.size(0), *self.input_shape)
+
+
+
+class Critic(nn.Module):
+    def __init__(self, input_shape):
+        super(Critic, self).__init__()
+        self.input_shape = input_shape
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(self.input_shape)), 1024),  # Increased layer size
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1)  # No Sigmoid for Wasserstein loss
+        )
+
+    def forward(self, returns):
+        validity = self.model(returns.view(returns.size(0), -1))
+        return validity
